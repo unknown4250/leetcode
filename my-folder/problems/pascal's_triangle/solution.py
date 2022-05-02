50ceef93c61ef54ba68be13bd2ebe8ccf14cbb37
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-02 07:47:33 +0000

    Sync LeetCode submission - Pascal's Triangle (python)

---
+++ my-folder/problems/pascal's_triangle/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = []
+
+        for i in range(numRows):
+            result.append([])
+
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    result[i].append(1)
+                else:
+                    result[i].append(result[i-1][j-1] + result[i-1][j])
+        return result
