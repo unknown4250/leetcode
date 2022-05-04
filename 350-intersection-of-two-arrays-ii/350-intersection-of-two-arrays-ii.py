b13d45f101530fcbf79878c635653b0d8741e06d
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-04 15:09:46 +0900

    Time: 35 ms (82.85%), Space: 13.5 MB (82.51%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
+        """
         # 공통된 중복값도 모두 처리해야 하므로 일반적인 교집합과는 다름
         # 값이 몇 개 들어있는지 알 수 있어야 중복을 제거하지 않고 처리할 수 있음
         # 따라서 Counter를 사용해서 배열의 원소 값 개수를 카운트하는 딕셔너리 필요
@@ -14,5 +15,20 @@ class Solution(object):
         for num in nums2:
             if num in counter.keys() and counter[num] > 0:
                 counter[num] -= 1
+                result.append(num)
+        return result
+        """
+        counter_dict = dict()
+        result = []
+
+        for num in nums1:
+            if num not in counter_dict:
+                counter_dict[num] = 1
+            else:
+                counter_dict[num] += 1
+
+        for num in nums2:
+            if num in counter_dict and counter_dict[num] > 0:
+                counter_dict[num] -= 1
                 result.append(num)
         return result
