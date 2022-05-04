31afe4f5bb0173ab604bda67d1e57a9031de3ad8
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-04 16:28:02 +0900

    Time: 170 ms (6.22%), Space: 13.3 MB (90.72%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,36 +1,29 @@
 class Solution(object):
     def isValidSudoku(self, board):
-        dict_row = {}
-        dict_column = {}
-        dict_grid = {}
-
         for i in range(9):
+            arr_row = []
+            arr_column = []
             for j in range(9):
                 # row
-                if board[i][j] != ".": 
-                    if board[i][j] not in dict_row:
-                        dict_row[board[i][j]] = 1
-                    else:
+                if board[i][j] != "." and board[i][j] in arr_row:
                         return False
+                else:
+                        arr_row.append(board[i][j])
                 # column
-                if board[j][i] != ".": 
-                    if board[j][i] not in dict_column:
-                        dict_column[board[j][i]] = 1
-                    else:
+                if board[j][i] != "." and board[j][i] in arr_column:
                         return False
-                # box
-                if i % 3 == 0 and j % 3 == 0:
-                    dict_grid.clear()
-                    for w in range(3):
-                        for h in range(3):
-                            if i + w < 9 and j + h < 9 and board[i+w][j+h] != ".":
-                                print(board[i+w][j+h])
-                                if board[i+w][j+h] not in dict_grid:
-                                    dict_grid[board[i+w][j+h]] = 1
-                                else:
-                                    return False
+                else:
+                        arr_column.append(board[j][i])
 
-            dict_row.clear()
-            dict_column.clear()
 
+        # grid
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                arr_grid = []
+                for w in range(3):
+                    for h in range(3):
+                        if board[i+w][j+h] != "." and board[i+w][j+h]in arr_grid:
+                            return False
+                        else:
+                            arr_grid.append(board[i+w][j+h])
         return True
