e484a15d07fdbfb7969d3fe8baa36430db8a269b
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-04 15:40:05 +0900

    Time: 43 ms (93.03%), Space: 14.3 MB (70.41%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -5,8 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """    
+        """
         # Brute force
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if target - nums[j] == nums[i]:
                     return [i, j]
+
+        """
+        dict = {nums[i]: i for i in range(len(nums))}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dict and dict[complement] != i:
+                return [i, dict[complement]]
