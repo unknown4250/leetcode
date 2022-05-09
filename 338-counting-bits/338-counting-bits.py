a426ea20cc3e0f925a19836a01860f48b476647c
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-09 13:11:53 +0900

    Time: 62 ms (91.87%), Space: 17.6 MB (7.12%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,8 +1,17 @@
 class Solution(object):
     def countBits(self, n):
         """
-        :type n: int
-        :rtype: List[int]
+        # 패턴 찾아내기
+        # i가 2의 n제곱이 될 때마다 offset 값 변경해야 함 
+        dp[0] = 0
+        dp[1] = dp[0] + 1 = dp[1-1] + 1
+        dp[2] = dp[0] + 1 = dp[2-2] + 1
+        dp[3] = dp[1] + 1 = dp[3-2] + 1
+        dp[4] = dp[0] + 1 = dp[4-4] + 1
+        dp[5] = dp[1] + 1 = dp[5-4] + 1
+        dp[6] = dp[2] + 1 = dp[6-4] + 1
+        dp[7] = dp[3] + 1 = dp[7-4] + 1
+        dp[8] = dp[0] + 1 = dp[8-8] + 1
         """
         offset = 1
         result = [0] * (n+1)
