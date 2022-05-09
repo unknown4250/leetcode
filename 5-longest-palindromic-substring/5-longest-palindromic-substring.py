c38091ee7726376dd30e227f03d4eef50ca34cde
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-09 16:17:41 +0900

    Time: 230 ms (95.44%), Space: 13.5 MB (88.44%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,15 +1,17 @@
 class Solution(object):
     def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
         if len(s) == 1: return s
 
         start, length = 0, 0
+
         for end in range(len(s)):
+            # 기존 Palindromic Substring에 end포인트의 문자를 포함해도 Palindrome했을 때, 조건 만족
+            # Ex. aa(a)
             if s[end-length:end+1] == s[end-length:end+1][::-1]:
                 start, length = end-length, length + 1
-            elif end-length >= 1 and s[end-length-1:end+1] == s[end-length-1:end+1][::-1]:
+            # 기존 Palindromic Substring에 end포인트의 문자와 Substring의 앞의 문자를 포함해도 Palindrome
+            # Ex. (b)aaa(b)
+            elif end - length >= 1 and s[end-length-1:end+1] == s[end-length-1:end+1][::-1]:
                 start, length = end - length - 1, length + 2
+
         return s[start:start+length]
