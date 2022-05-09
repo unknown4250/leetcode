c1abf701f0eaea55a364af0134e2078f31eb7d75
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-09 14:38:48 +0900

    Time: 12 ms (97.23%), Space: 13.7 MB (51.82%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -7,8 +7,7 @@ class Solution(object):
         """
         i = 0
         j = 0
-        if len(s) == 0:
-            return True
+
         while i < len(s) and j < len(t):
             if s[i] == t[j]:
                 i += 1
