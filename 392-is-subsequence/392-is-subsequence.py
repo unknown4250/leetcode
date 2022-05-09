adcba6e8509ce1d40beac36d9cb21a7a94e6f493
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-09 14:46:50 +0900

    Time: 28 ms (44.54%), Space: 13.6 MB (51.82%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -5,6 +5,10 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        # dynamic programming
+        # https://leetcode.com/problems/is-subsequence/discuss/678389/Python-3-Solutions%3A-DP-2-pointers-and-follow-up-BS-explained
+   
+        # two pointers
         i = 0
         j = 0
 
