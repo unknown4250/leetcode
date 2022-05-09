079d43f449e4888add30c7c9f7b81eb810cf75e5
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-09 15:43:15 +0900

    Time: 47 ms (21.72%), Space: 13.5 MB (22.62%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        """
         cnt = 0
         while n > 1:
             x = 0
@@ -15,3 +16,9 @@ class Solution(object):
             cnt += 1
 
         return False if cnt % 2 == 0 else True 
+        """
+        result = [0] * 1001
+        for i in range(2, n + 1):
+            result[i] = result[i-1] + 1
+
+        return False if result[n] % 2 == 0 else True
