7793e9144b67691d9c612f439ae37bdc80d74bc5
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-09 15:24:20 +0900

    Time: 39 ms (91.53%), Space: 13.5 MB (79.35%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        """
+        # [10,15,20] = (시작) → 10 → 15 → 20 → (종료)
+        # 한 번에 1개 혹은 2개의 계단을 오를 수 있음
+        # 가능한 경우의 수 : (10, 15, 20), (10, 15), (10, 20), (15, 20), (15)
+        """
+        min_cost = [0] * len(cost)
+        min_cost[0] = cost[0]
+        min_cost[1] = cost[1]
+
+        for i in range(2, len(cost)):
+            min_cost[i] = min(min_cost[i-2] + cost[i], min_cost[i-1] + cost[i])
+
+        return min(min_cost[len(cost) - 1], min_cost[len(cost) - 2])
