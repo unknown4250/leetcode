54a7174a7d302591a1f097f78dee86b8d4713406
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-10 11:52:16 +0900

    Time: 725 ms (20.70%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,3 +1,3 @@
-SELECT employee_id, IF (MOD(employee_id,2) = 0 OR name LIKE 'M%', 0, salary) AS bonus
+SELECT employee_id, IF (name LIKE 'M%' OR MOD(employee_id,2) = 0, 0, salary) AS bonus
 FROM Employees
 ORDER BY employee_id
