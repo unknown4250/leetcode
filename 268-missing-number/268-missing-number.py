99622d85ddd7c867921e6e7cc2edc3026006cfd3
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-11 17:11:40 +0900

    Time: 116 ms (77.15%), Space: 15.1 MB (14.48%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -4,4 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return list(set([i for i in range(len(nums) + 1)]) - set(nums))[0]
+        #return list(set([i for i in range(len(nums) + 1)]) - set(nums))[0]
+        set_nums = set(nums)
+
+        for i in range(len(nums) + 1):
+            if i not in set_nums:
+                return i
