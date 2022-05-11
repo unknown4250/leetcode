8e0152d9d589485b2da06a4929c03966d769d228
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-11 10:56:08 +0900

    Time: 696 ms (41.64%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,1 +1,1 @@
-SELECT user_id,  CONCAT(UPPER(SUBSTRING(name, 1, 1)),  LOWER(SUBSTRING(name, 2))) as name FROM Users ORDER BY user_id
+SELECT user_id,  CONCAT(UPPER(SUBSTRING(name, 1, 1)),  LOWER(SUBSTRING(name, 2))) as name FROM Users ORDER BY 1
