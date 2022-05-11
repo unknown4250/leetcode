02312c85b87b12184154b724890449ab946a4dc0
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-11 16:59:15 +0900

    Time: 27 ms (98.43%), Space: 13.9 MB (63.10%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -6,4 +6,22 @@ class Solution(object):
         :rtype: bool
         """
         
-        return sorted(s) == sorted(t)
+        # return sorted(s) == sorted(t)
+        dict = {}
+
+        for ch in s:
+            if ch in dict:
+                dict[ch] += 1
+            else:
+                dict[ch] = 1
+
+        for ch in t:
+            if ch in dict:
+                dict[ch] -= 1
+            else:
+                return False
+
+        for val in dict.values():
+            if val != 0:
+                return False
+        return True
