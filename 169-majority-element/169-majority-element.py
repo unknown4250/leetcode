8b8888f29726166fe2662372925bc76d81f3947c
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-11 16:30:02 +0900

    Time: 150 ms (69.04%), Space: 15 MB (31.51%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -6,12 +6,12 @@ class Solution(object):
         """
         dict = {}
         for n in nums:
-            dict[n] = dict.get(n, 0) + 1
-            """
+            #dict[n] = dict.get(n, 0) + 1
+            
             if n in dict:
                 dict[n] += 1
             else:
                 dict[n] = 1
-            """
+            
             if dict[n] > len(nums) / 2:
                 return n
