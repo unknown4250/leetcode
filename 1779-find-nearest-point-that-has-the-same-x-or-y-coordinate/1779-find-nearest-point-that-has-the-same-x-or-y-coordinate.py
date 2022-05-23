c480490a013e4405bed32d2f1918babcf66ab07f
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-23 12:59:03 +0900

    Time: 879 ms (25.72%), Space: 19.1 MB (9.26%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -6,17 +6,12 @@ class Solution(object):
         :type points: List[List[int]]
         :rtype: int
         """
-        
         smallest_idx = -1
         smallest_dist = float("inf")
         for i in range(len(points)):
             if points[i][0] == x or points[i][1] == y:
                 temp_dist = abs(x-points[i][0]) + abs(y-points[i][1]) 
                 if temp_dist < smallest_dist:
-                    smallest_dist = temp_dist
-                    smallest_idx = i
+                    smallest_dist, smallest_idx = temp_dist, i
         
-        if smallest_idx > -1:
-            return smallest_idx
-        else:
-            return -1
+        return smallest_idx
