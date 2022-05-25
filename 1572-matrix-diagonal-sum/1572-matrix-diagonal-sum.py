ba6f1249599bd32298b46c0269678beea68e3061
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-25 11:58:42 +0900

    Time: 101 ms (61.52%), Space: 13.9 MB (5.99%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type mat: List[List[int]]
         :rtype: int
         """
+        """
         sum, j = 0, 0
         mat_len, arr_len = len(mat), len(mat[0])
 
@@ -19,3 +20,14 @@ class Solution(object):
                 sum += (mat[i][j] + mat[i][arr_len-1-j] + mat[len(mat) -1 -i][j] + mat[len(mat) -1 -i][arr_len-1-j])
             j += 1
         return sum
+        """
+        res = 0
+        n = len(mat)
+        for i in range(len(mat)):
+            res += mat[i][i]
+            res += mat[n-1-i][i]
+        
+        if n % 2 == 1:
+            res -= mat[n//2][n//2]
+
+        return res
