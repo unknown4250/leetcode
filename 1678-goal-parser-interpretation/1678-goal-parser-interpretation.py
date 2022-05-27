33312df0a6f58d46dcd52058d887dd65a04116df
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-27 13:27:32 +0900

    Time: 31 ms (19.35%), Space: 13.6 MB (31.57%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -4,5 +4,20 @@ class Solution(object):
         :type command: str
         :rtype: str
         """
-        return command.replace("()", "o").replace("(al)", "al")
+        #return command.replace("()", "o").replace("(al)", "al")
+        
+        res = ""
+        idx = 0
+
+        while idx < len(command):
+            if command[idx] == "G":
+                res += "G"
+                idx += 1
+            if command[idx:idx+2] == "()":
+                res += "o"
+                idx += 2
+            if command[idx:idx+4] == "(al)":
+                res += "al"
+                idx += 4
+        return res
         
