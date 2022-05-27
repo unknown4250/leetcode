00a4a2c44ff79c347de2b940dce334d86ab3454b
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-05-27 13:24:19 +0900

    Time: 19 ms (73.93%), Space: 13.7 MB (8.15%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def interpret(self, command):
+        """
+        :type command: str
+        :rtype: str
+        """
+        command = command.replace("()", "o")
+        command = command.replace("(al)", "al")
+        return command
+        
