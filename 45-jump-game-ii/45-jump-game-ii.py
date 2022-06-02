2f6a367c6f16e793607c9b140d94b06a480d4c6a
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-02 19:05:35 +0900

    Time: 119 ms (72.88%), Space: 14.1 MB (96.34%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l = r = cnt = 0
+
+
+        while r < len(nums) - 1:
+            cnt += 1
+            next_loc = max([i + nums[i] for i in range(l, r + 1)])
+            l, r = r + 1, next_loc
+
+        return cnt
