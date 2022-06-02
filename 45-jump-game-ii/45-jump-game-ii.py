37c87e8ddc2e564d631bc22721d4d2be7ec89d32
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-02 19:07:04 +0900

    Time: 167 ms (53.08%), Space: 14.2 MB (89.63%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        
         l = r = cnt = 0
-
-
+        
         while r < len(nums) - 1:
             cnt += 1
             next_loc = max([i + nums[i] for i in range(l, r + 1)])
             l, r = r + 1, next_loc
-
+        
         return cnt
