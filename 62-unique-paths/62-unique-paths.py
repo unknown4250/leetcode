687a6ad1d036e92ea13da36f6e5b79fd0fee7cc5
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-08 10:48:15 +0900

    Time: 26 ms (47.40%), Space: 13.3 MB (88.43%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """        
+        dp = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[i][j] = 1
+                elif i == 0 and j != 0:
+                    dp[i][j] += dp[i][j-1]
+                elif i != 0 and j == 0:
+                    dp[i][j] += dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
