cee9e28e55f9fd0a27431d849b1a78b8b26bdb99
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-08 10:38:53 +0900

    Time: 108 ms (46.66%), Space: 14.6 MB (77.97%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -4,20 +4,17 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        dp = grid
-    
+        
         row, col = len(grid), len(grid[0])
 
         for i in range(row):
             for j in range(col):
                 if i == 0 and j == 0:
-                    dp[i][j] = (grid[i][j])
-                    continue
+                    grid[i][j] = grid[i][j]
                 elif i == 0 and j != 0:
-                    dp[i][j] = (dp[i][j-1] + grid[i][j])
-                elif j == 0 and i != 0:
-                    dp[i][j] = (dp[i-1][j] + grid[i][j])
+                    grid[i][j] += grid[i][j-1]
+                elif i != 0 and j == 0:
+                    grid[i][j] += grid[i-1][j]
                 else:
-                    dp[i][j] = (min(dp[i][j-1], dp[i-1][j]) + grid[i][j])
-
-        return dp[row-1][col-1]
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        return grid[row-1][col-1]
