4169c482602fe65dbde701fe92e8803779b6f59b
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-09 15:44:26 +0900

    Time: 691 ms (77.07%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,6 +1,5 @@
-select u.name, ifnull(sum(r.distance), 0) as travelled_distance
-from users u
-left join rides r
-on u.id = r.user_id
-group by r.user_id
-order by travelled_distance desc, u.name asc
+SELECT name, IFNULL(SUM(distance),0) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r ON u.id = user_id
+GROUP BY user_id
+ORDER BY travelled_distance DESC, name
