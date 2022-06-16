a01f687a3989b07ec6954c9b0daab1e7394acf78
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-16 11:10:38 +0900

    Time: 40 ms (74.29%), Space: 13.6 MB (57.84%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -1,34 +1,16 @@
 class Solution(object):
     def intersect(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        """
-        # 공통된 중복값도 모두 처리해야 하므로 일반적인 교집합과는 다름
-        # 값이 몇 개 들어있는지 알 수 있어야 중복을 제거하지 않고 처리할 수 있음
-        # 따라서 Counter를 사용해서 배열의 원소 값 개수를 카운트하는 딕셔너리 필요
-        counter = Counter(nums1)
-
-        result = []
-        for num in nums2:
-            if num in counter.keys() and counter[num] > 0:
-                counter[num] -= 1
-                result.append(num)
-        return result
-        """
-        counter_dict = dict()
-        result = []
-
-        for num in nums1:
-            if num not in counter_dict:
-                counter_dict[num] = 1
+        
+        nums1, nums2 = sorted(nums1), sorted(nums2)
+        idx1 = idx2 = 0
+        res = []
+        
+        while idx1 < len(nums1) and idx2 < len(nums2):
+            if nums1[idx1] < nums2[idx2]:
+                idx1 += 1
+            elif nums1[idx1] > nums2[idx2]:
+                idx2 += 1
             else:
-                counter_dict[num] += 1
-
-        for num in nums2:
-            if num in counter_dict and counter_dict[num] > 0:
-                counter_dict[num] -= 1
-                result.append(num)
-        return result
+                res.append(nums1[idx1])
+                idx1, idx2 = idx1 + 1, idx2 + 1
+        return res
