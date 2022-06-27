ac97854f6e7aa72dacd241ace61abce5093a2959
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-27 11:05:03 +0900

    Time: 37 ms, Memory: 13.5 MB - LeetHub

---
+++ reverse-integer/reverse-integer.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x > 0:
+            result = int(str(x)[::-1])
+        else:
+            result = int(str(x*-1)[::-1]) * -1
+        
+        if result >= 2**31 or result <= -2**31:
+            return 0
+        else:
+            return result
