09b4cd52b995f4d7f0641c110ef698a113f7c286
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-27 11:03:41 +0900

    Time: 110 ms, Memory: 13.5 MB - LeetHub

---
+++ valid-sudoku/valid-sudoku.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        for i in range(9):
+            arr_row = []
+            arr_column = []
+            for j in range(9):
+                # row
+                if board[i][j] != "." and board[i][j] in arr_row:
+                        return False
+                arr_row.append(board[i][j])
+                # column
+                if board[j][i] != "." and board[j][i] in arr_column:
+                        return False
+                arr_column.append(board[j][i])
+
+
+        # grid
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                arr_grid = []
+                for w in range(3):
+                    for h in range(3):
+                        if board[i+w][j+h] != "." and board[i+w][j+h]in arr_grid:
+                            return False
+                        arr_grid.append(board[i+w][j+h])
+        return True
