58a2c0cdb2228c4c85ddc6e15d0815aaf5bff06b
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-29 14:17:33 +0900

    Time: 409 ms (46.46%), Space: 15.8 MB (59.06%) - LeetHub

--- 594-longest-harmonious-subsequence/594-longest-harmonious-subsequence.py
+++ 594-longest-harmonious-subsequence/594-longest-harmonious-subsequence.py
@@ -8,7 +8,5 @@ class Solution(object):
 
         for num in nums:
             if num + 1 in num_counts:
-                temp = num_counts[num] + num_counts[num+1]
-                if res < temp:
-                    res = temp
+                res = max(res, num_counts[num] + num_counts[num+1])
         return res
