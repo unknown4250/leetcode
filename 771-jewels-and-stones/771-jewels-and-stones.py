17c51639c12387181dd0b911349ddf367e1e5628
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-06-29 15:01:43 +0900

    Time: 15 ms (92.73%), Space: 13.7 MB (6.26%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,8 +1,4 @@
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
 
-        res = 0
-        for stone in stones:
-            if stone in jewels:
-                res += 1
-        return res
+        return sum(stones.count(ch) for ch in jewels)
