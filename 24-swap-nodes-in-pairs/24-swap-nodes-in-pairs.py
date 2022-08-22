998025471c3ef3b12811cff139ea4fed0e87ff2a
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-08-22 18:22:00 +0900

    Time: 37 ms (17.62%), Space: 13.3 MB (80.35%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -9,11 +9,23 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        cur = head
         
-        while cur and cur.next:
-            # just change values
-            cur.val, cur.next.val = cur.next.val, cur.val
-            cur = cur.next.next
+        root = prev = ListNode(head)
+        prev.next = head
         
-        return head
+        while head and head.next:
+            # 'b' points 'a(head)'
+            b = head.next
+            head.next = b.next
+            b.next = head
+            
+            # 'prev'(=previous nodes of a) points 'b'
+            prev.next = b
+            
+            # move for the next comparison
+            head = head.next
+            prev = prev.next.next
+        
+        # 연결 리스트의 head를 가리키는 노드가 직접 바뀌므로
+        # head를 리턴하지 못하고 head의 이전 값인 root의 next를 리턴하게 됨
+        return root.next
