ab8134f08bfa169912a4ca414c1616d423a175ec
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-10-09 17:29:58 +0900

    Time: 83 ms (27.58%), Space: 14.1 MB (23.34%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        prev_elements = []
+        
+        def dfs(elements):
+            # 리프 노드일 때(조합 결과 길이 = 입력 배열 길이) 결과 추가
+            if len(elements) == 0:
+                results.append(prev_elements[:])
+            
+            # 순열 생성 재귀 호출
+            for e in elements:
+                next_elements = elements[:]
+                next_elements.remove(e)
+                prev_elements.append(e)
+                
+                dfs(next_elements)
+                prev_elements.pop()
+        
+        dfs(nums)
+        return results
