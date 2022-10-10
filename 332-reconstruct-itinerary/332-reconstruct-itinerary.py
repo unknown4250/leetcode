36e938ce1ccf6aaaad91c86ac8449ee2be73a345
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-10-10 15:57:38 +0900

    Time: 99 ms (83.71%), Space: 14.4 MB (88.50%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -4,7 +4,12 @@ class Solution:
         graph = collections.defaultdict(list)
         
         # 어휘순 정렬 후, 그래프 순서대로 구성
+        """
+        # 큐로 구성한 버전
         for a, b in sorted(tickets):
+            graph[a].append(b)
+        """
+        for a, b in sorted(tickets, reverse=True):
             graph[a].append(b)
         
         route = []
@@ -12,7 +17,8 @@ class Solution:
         def dfs(a):
             # 어휘 순으로 방문하기 때문에 첫 번째 값 읽어야 함
             while graph[a]:
-                dfs(graph[a].pop(0))
+                # dfs(graph[a].pop(0))
+                dfs(graph[a].pop())
             route.append(a)
         
         dfs('JFK')
