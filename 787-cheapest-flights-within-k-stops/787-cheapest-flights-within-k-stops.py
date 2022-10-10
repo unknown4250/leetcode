e269aded51eadb239bbe36cc2e13da4bc334cbbe
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-10-10 19:49:14 +0900

    Time: 113 ms (94.96%), Space: 15.4 MB (25.40%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -9,7 +9,7 @@ class Solution:
         
         # 그래프에 loop 있는 경우, time limit 발생함
         # 최소 비용으로 특정 노드에 가기까지 남아있는 횟수 기록
-        visited = [0] * n
+        visited = [False] * n
         
         while my_queue:
             price, node, stops = heapq.heappop(my_queue)
@@ -17,11 +17,14 @@ class Solution:
             if node == dst:
                 return price
             
+            # 이전에 방문했던 노드라면 현재 방문할 필요가 없음
+            # 방문 횟수가 더 많이 남고, 비용도 더 적은 상태에서 방문했으니까
             if visited[node] >= stops:
                 continue
             
             visited[node] = stops
             
+
             for v, w in graph[node]:
                 heapq.heappush(my_queue, (price + w, v, stops-1))
         
