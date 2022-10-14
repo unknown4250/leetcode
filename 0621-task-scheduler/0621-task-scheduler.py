b70db1b485a810e6c060e6e29006cc3b0808e091
unknown4250 <74250200+unknown4250@users.noreply.github.com>
2022-10-14 20:59:07 +0900

    Time: 447 ms (88.61%), Space: 15.5 MB (87.95%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        """
+        :type tasks: List[str]
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return len(tasks)
+
+        dict = {}
+
+        # 카운터 만들기
+        for task in tasks:
+            dict[task] = dict.get(task, 0) + 1
+
+        counts_list = [dict[task] for task in dict]
+        max_count = max(counts_list)
+
+        # 가장 많은 task는 여러 개일 수 있음 (예를 들어, 'A' 3개, 'B' 3개)
+        max_count_tasks = counts_list.count(max_count)
+
+        result = max_count + (max_count - 1) * n + (max_count_tasks) - 1
+        return max(len(tasks), result)
