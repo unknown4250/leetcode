69f5533345442e48f80f5ab326ca6381d88e0a96
unknown4250 <mjjj4300@gmail.com>
2022-11-17 15:22:53 +0900

    Time: 1085 ms (72.59%), Space: 17.1 MB (24.27%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -6,28 +6,29 @@ class Solution(object):
         """
         res = []
 
-        # 오름차순 정렬하는 게 좋음
-        # 1. 양수 나오면 그 이후는 체크할 필요 없음
-        # 2. 중복된 숫자는 인덱스만 계속 옮겨서 지나갈 수 있음
+        # 오름차순 정렬
         nums.sort()
 
         # 최소 값(nums[i])은 고정하고 합이 0이 되는 나머지 두 숫자 찾기
         # 투-포인터 방식
         for i in range(len(nums) - 2):
+            
+            # 양수 나오면 그 이후는 체크할 필요 없음
             if nums[i] > 0:
                 break
-                
-            left, right = i + 1, len(nums) - 1
-
-            
             if i > 0 and nums[i] == nums[i-1]:
                 continue
+            
+            # 왼쪽, 오른쪽 포인터
+            left, right = i + 1, len(nums) - 1
 
             while left < right:
+                # 세 수의 합
                 sum = nums[i] + nums[left] + nums[right]
-
+                # 합이 0보다 크면, 가장 큰 값 변경
                 if sum > 0:
                     right -= 1
+                # 합이 0보다 작으면, 가장 작은 값 변경
                 elif sum < 0:
                     left += 1
                 else:
