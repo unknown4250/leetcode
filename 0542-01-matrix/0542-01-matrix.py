694de3f18a0fae16654e3c0f414680934868125b
unknown4250 <mjjj4300@gmail.com>
2022-11-29 14:13:20 +0900

    Time: 2128 ms (8.16%), Space: 16.7 MB (53.47%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,32 +1,43 @@
 class Solution(object):
     def updateMatrix(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: List[List[int]]
+        """
         m, n = len(mat), len(mat[0])
-
-        visited = [[False] * n for _ in range(m)]
         queue = deque()
 
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
+        visited = [[False] * n for _ in range(m)]
+
+
+        for i in range(m):
+            for j in range(n):
+                # 값이 0인 셀을 기준으로 탐색 시작
                 if mat[i][j] == 0:
-                    queue.append((i, j, 0))
-                    visited[i][j] = True
+                    queue.append((0, i, j)) # 큐에 삽입 (거리, y좌표, x좌표)
+                    visited[i][j] = True    # 방문 표시
+
 
         while queue:
-            for _ in range(len(queue)):
-                x, y, dist = queue.popleft()
+            distance, x, y = queue.popleft()
 
-                for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-                    nx = x + dx
-                    ny = y + dy
+            # 현재 셀의 상하좌우 탐색
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nx = x + dx
+                ny = y + dy
 
-                    if nx < 0 or nx >= m or ny < 0 or ny >= n:
-                        continue
+                # 행렬 범위 벗어나면 제외
+                if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                    continue
+                
+                # 방문하지 않은 셀인 경우
+                if not visited[nx][ny]:
+                    # 현재 셀의 방문 표시
+                    visited[nx][ny] = True
+                    # 이후에 현재 셀의 주변을 탐색하기 위해 큐에 삽입
+                    queue.append((distance+1, nx, ny))
+                    # 0으로부터의 행렬에 직접 입력
+                    mat[nx][ny] = distance+1
 
-                    if not visited[nx][ny]:
-                        visited[nx][ny] = True
-                        queue.append((nx, ny, dist+1))
-                        mat[nx][ny] = dist+1
-
-        
         return mat
         
