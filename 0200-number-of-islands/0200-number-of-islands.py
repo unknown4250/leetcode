f4045b8bc6e4432ca684751d885640040ab24f9e
unknown4250 <mjjj4300@gmail.com>
2022-12-01 15:41:54 +0900

    Time: 704 ms (47.25%), Space: 28.6 MB (93.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,47 +1,71 @@
 class Solution(object):
+    """
+    # DFS 버전
     def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+        answer = 0
 
-        # 그리드의 세로, 가로 길이
-        m, n = len(grid), len(grid[0])
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    answer += 1
+        return answer
 
-        # 결과 값
-        count = 0
+    def dfs(self, grid, x, y):
+        # 입력 배열 범위를 벗어나는 경우, dfs 수행 X
+        if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]):
+            return
+        
+        # 섬이 아닌 경우, dfs 수행 X
+        if grid[x][y] != "1":
+            return
+
+        # 해당 위치를 재탐색하지 않기 위해 "1" → "0"으로 값 변경
+        grid[x][y] = "0"
+
+        # 현재 위치에서 상하좌우를 탐색
+        for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+            nx, ny = x+dx, y+dy
+            self.dfs(grid, nx, ny)
+    """
+    def numIslands(self, grid):
 
         # bfs 수행하기 위한 큐 선언
-        queue = deque([])
+        queue = deque()
 
-        for i in range(m):
-            for j in range(n):
-                # 섬인 위치에 대해서만 탐색
-                if grid[i][j] == '1':
+        answer = 0 
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                # 섬인 위치만 탐색
+                if grid[i][j] == "1":
                     # 다시 탐색하지 않기 위해 다른 값으로 변경
-                    grid[i][j] = '0'
+                    grid[i][j] = "0"
 
-                    # 한 섬의 시작점
-                    queue.append((i, j))
+                     # 한 섬의 시작점
+                    queue.append((i,j))
 
                     while queue:
+                        # 현재 위치
                         x, y = queue.popleft()
 
                         # 현재 위치를 중심으로 상하좌우 탐색
                         for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-                            nx = x + dx
-                            ny = y + dy
+                            nx, ny = x+dx, y+dy
 
-                            # 그리드 영역 벗어나거나 섬이 아닌 경우 제외
-                            if nx < 0 or nx >= m or ny < 0 or ny >= n or grid[nx][ny] != '1':
+                             # 그리드 영역 벗어나는 경우 제외
+                            if nx < 0 or nx >= len(grid) or ny < 0 or ny >= len(grid[0]):
                                 continue
                             
-                            # 이후에 탐색할 중심점 위치
-                            queue.append((nx, ny))
-                            
-                            # 방문 표시
-                            grid[nx][ny] = '0'
+                            # 새로운 위치가 섬인 경우
+                            if grid[nx][ny] == "1":
+                                # 이후에 탐색할 위치
+                                queue.append((nx, ny))
 
+                                # 방문 표시
+                                grid[nx][ny] = "0"
+                                
                     # while문 종료되면 하나의 섬 탐색이 종료된 것 (섬 개수 +1)
-                    count += 1
-        return count
+                    answer += 1
+
+        return answer
