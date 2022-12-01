75c98842fe76c423cd0614d51c26aded3397fc39
unknown4250 <mjjj4300@gmail.com>
2022-12-01 14:33:29 +0900

    Time: 76 ms (92.45%), Space: 16.5 MB (28.56%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,46 +1,31 @@
 class Solution(object):
     def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
         graph = defaultdict(list)
 
-        # 강의 정보 입력
         for a, b in prerequisites:
             graph[a].append(b)
         
-        # 순환 여부, 방문 여부 확인하기 위한 set
-        traced, visited = set(), set()
-                
-        def dfs(node):
-            # 방문 내역에 남아있으면 순환구조
-            if node in traced:
+        visited, traced = [False] * numCourses, [False] * numCourses
+
+        def dfs(x):
+            if traced[x]:
                 return False
-
-            # 이미 방문한 노드라면 제외(가지치기)
-            if node in visited:
+            
+            if visited[x]:
                 return True
+            
+            traced[x] = True
 
-            # 현재 노드를 방문 내역에 추가
-            traced.add(node)
-
-            # 현재 강의의 선행 강의들의 DFS 수행
-            for course in graph[node]:
+            for course in graph[x]:
                 if not dfs(course):
                     return False
             
-            # 제거하지 않으면 같은 선행 강의를 갖는 다른 강의가 순환 구조로 판단될 수 있음
-            traced.remove(node)
-            
-            # 방문 표시
-            visited.add(node)
+            traced[x] = False
+            visited[x] = True
 
             return True
         
-        for x in list(graph):
-            if not dfs(x):
+        for course in list(graph):
+            if not dfs(course):
                 return False
-        
         return True
