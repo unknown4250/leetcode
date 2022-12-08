2d03660bb5beaa3302a953ade490ac4365e2ec66
unknown4250 <mjjj4300@gmail.com>
2022-12-08 14:30:28 +0900

    Update 78-subsets.py
    combinations 사용한 버전 추가

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,5 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        """
+        # 조합 사용한 버전
+        answer = []
+
+        for i in range(len(nums)+1):
+            for combi in combinations(nums, i):
+                answer.append(list(combi))
+
+        return answer       
+        """
         results = []
         
         def dfs(idx, path):
