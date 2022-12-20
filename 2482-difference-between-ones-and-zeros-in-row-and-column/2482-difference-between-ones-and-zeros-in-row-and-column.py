2e326af745ad18366284511f69f7bae98ec99cf1
unknown4250 <mjjj4300@gmail.com>
2022-12-20 16:26:23 +0900

    Time: 3810 ms (39.01%), Space: 55.8 MB (58.16%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -5,9 +5,8 @@ class Solution(object):
         # 각 행, 열에 1이 몇 개씩 있는지 카운팅
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    one_rows[i] += 1
-                    one_cols[j] += 1
+                one_rows[i] += grid[i][j]
+                one_cols[j] += grid[i][j]
         
         # 결과 배열
         diff = [[0] * len(grid[0]) for _ in range(len(grid))]
@@ -18,3 +17,4 @@ class Solution(object):
                 diff[i][j] = one_rows[i] + one_cols[j] - (len(grid) - one_rows[i]) - (len(grid[0]) - one_cols[j])
         
         return diff
+        
