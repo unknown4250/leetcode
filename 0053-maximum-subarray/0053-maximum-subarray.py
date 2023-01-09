b4a3f51f01dee08f46b6e54443ad534b4d84cfcd
unknown4250 <mjjj4300@gmail.com>
2023-01-09 16:27:55 +0900

    Time: 583 ms (83.86%), Space: 27.1 MB (5.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+        # 서브 배열의 최대 합을 저장할 배열
+        sums = [0] * len(nums)
+
+        # 배열 첫 번째 요소 초기화
+        sums[0] = nums[0]
+
+        # 입력 배열의 현재 값과, 이전까지의 합을 비교
         for i in range(1, len(nums)):
-            nums[i] = max(nums[i-1]+nums[i], nums[i])
+            sums[i] = max(nums[i], sums[i-1]+nums[i])
         
-        return max(nums)
+        return max(sums)
